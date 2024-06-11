726e9456898842a16afd341481ec9befe59755dd
WongShiFong <shifong.wong@57codebox.com>
2024-06-11 14:36:30 +0800

    title styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'api-monitoring';
+  // title = 'api-monitoring';
+  title: string = '';
+
+  private titles: { [key: string]: string } = {
+    '/documentation': 'Documentation',
+    // Add more routes and titles as needed
+  };
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.title = this.titles[event.urlAfterRedirects] || 'API Monitoring Dashboard';
+    });
+  }
 }
