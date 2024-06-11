a2d38490642a1483437ec2c8b7a4cdb55f877ce0
ipanropi <irfan100053@gmail.com>
2024-06-11 12:12:58 +0800

    Add home and documentation component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
+import {AppComponent} from "./app.component";
+import {DocumentationComponent} from "./component/documentation/documentation.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'documentation', component: DocumentationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { DocumentationComponent } from './component/documentation/documentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DocumentationComponent
   ],
   imports: [
     BrowserModule,
