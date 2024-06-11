6819905020ce8d33a0e5f25d35b9078092bce9e3
WongShiFong <shifong.wong@57codebox.com>
2024-06-11 16:08:45 +0800

    add file component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DocumentationComponent} from "./component/documentation/documentation.component";
 import {DashboardComponent} from "./component/dashboard/dashboard.component";
+import {DocumentationAddNewFileComponent} from "./component/documentation/documentation-add-new-file/documentation-add-new-file.component";
 import {SettingsComponent} from "./component/settings/settings.component";
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent},
   { path: 'settings', component: SettingsComponent},
-  { path: 'documentation', component: DocumentationComponent}
+  { path: 'documentation', component: DocumentationComponent},
+  { path: 'documentation/newfile', component: DocumentationAddNewFileComponent},
 ];
 
 @NgModule({
