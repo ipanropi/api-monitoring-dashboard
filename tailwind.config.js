f728eaefd87e16a102e08affe32b23e8c45cdc5f
ipanropi <irfan100053@gmail.com>
2024-06-13 11:27:08 +0800

    Resolve Mobile Responsive Issue for Left menu, Settings, Reporting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  extend: {
+    width: {
+      'unset': 'unset',
+    },
+    plugins: [],
+  }
 }
 
