99e2557405cdfd1142f4b7d669facd3ffdfe7c17
ipanropi <irfan100053@gmail.com>
2024-06-13 17:14:06 +0800

    Documentation Table responsive resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,11 @@ module.exports = {
   important: true,
   content: ["./src/**/*.{html,js}"],
   extend: {
-    width: {
-      'unset': 'unset',
+    gridTemplateColumns: {
+      '16': 'repeat(16, minmax(0, 1fr))',
+      '14': 'repeat(14, minmax(0, 1fr))',
+      '15': 'repeat(15, minmax(0, 1fr))'
+      // You can add more custom column counts if needed
     },
     plugins: [],
   }
